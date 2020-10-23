82447be4dfc86e97fd6bd1a19cfb2d45cc494982
DylanonWicchiramala <43574914+DylanonWicchiramala@users.noreply.github.com>
2020-10-23 21:24:39 +0700

    predict other axis(bugfix,reformated file)

--- regression.py
+++ regression.py
@@ -4,9 +4,10 @@ import pylab
 import statistics as stat
 
 DataSetxi = [10, 23, 32, 44, 62, 69]
-DataSetyi = [100, 96, 94, 91 ,86, 83]
+DataSetyi = [100, 96, 94, 91, 86, 83]
 
 fig, ax = plt.subplots()
+
 
 def sumMaltipy(mx, my):
     sum = 0
@@ -14,11 +15,13 @@ def sumMaltipy(mx, my):
         sum += mx[i] * my[i]
     return sum
 
+
 def sumSquare(mx):
     sum = 0
     for i in range(len(mx)):
         sum += mx[i] * mx[i]
     return sum
+
 
 class regression:
 
@@ -29,14 +32,12 @@ class regression:
              (sumSquare(DataSetxi) - N * stat.mean(DataSetxi) * stat.mean(DataSetxi))
         w0 = stat.mean(DataSetyi) - w1 * stat.mean(DataSetxi)
 
-        if(axis== 'x' or axis is None):
+        if (axis == 'x' or axis is None):
             y = w1 * val + w0
             print("x is ", val, "predict y is ", y)
-        elif(axis == 'y'):
+        elif (axis == 'y'):
             x = (val - w0) / w1
-            print("y is ", val,"predict x is", x)
-
-
+            print("y is ", val, "predict x is", x)
 
     @staticmethod
     def liner_plot(mx, my):
@@ -46,16 +47,17 @@ class regression:
         w0 = stat.mean(my) - w1 * stat.mean(mx)
         x = np.linspace(min(mx) - 1, max(mx) + 2)
         y = w1 * x + w0
-        #ax.grid(axis='both')
+        # ax.grid(axis='both')
         plt.title(label='regression')
         ax.plot(x, y)
-        #ax.plot(mx, my, 'ro')
+        # ax.plot(mx, my, 'ro')
 
     @staticmethod
-    def data_plot(mx ,my):
-        #ax.grid(axis='both')
+    def data_plot(mx, my):
+        # ax.grid(axis='both')
         plt.title(label='reg')
         ax.plot(mx, my, 'ro')
+
 
 ax.grid(axis='both')
 
