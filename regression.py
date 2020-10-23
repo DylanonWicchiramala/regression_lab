35d31eac5f3d45776b4442e26101e37c2b5a71bf
DylanonWicchiramala <43574914+DylanonWicchiramala@users.noreply.github.com>
2020-10-23 20:50:32 +0700

    gird bug fig

--- regression.py
+++ regression.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import pylab
 import statistics as stat
 
-DataSetxi = [1, 3, 2, 5, 7, 7]
-DataSetyi = [1, 4, 7, 8, 12, 13]
+DataSetxi = [10, 23, 32, 44]
+DataSetyi = [100, 96, 94, 91]
 
 fig, ax = plt.subplots()
 
@@ -39,14 +39,14 @@ class regression:
         w0 = stat.mean(my) - w1 * stat.mean(mx)
         x = np.linspace(min(mx) - 1, max(mx) + 2)
         y = w1 * x + w0
-        ax.grid(axis='both')
+        #ax.grid(axis='both')
         plt.title(label='regression')
         ax.plot(x, y)
         #ax.plot(mx, my, 'ro')
 
     @staticmethod
     def data_plot(mx ,my):
-        ax.grid(axis='both')
+        #ax.grid(axis='both')
         plt.title(label='reg')
         ax.plot(mx, my, 'ro')
 
@@ -62,7 +62,9 @@ class regression:
         ax.plot(x, y, color='lightgray')
         #ax.plot(mx, my, 'ro')  #dataplot
 
+ax.grid(axis='both')
+
 regression.liner_plot(DataSetxi, DataSetyi)
 regression.data_plot(DataSetxi, DataSetyi)
-regression.liner_plot2(DataSetxi, DataSetyi)
+#regression.linear_predict(60)
 pylab.show()
