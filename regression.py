1733af29b48cd8c092da3f615105d0bb9596cb0f
DylanonWicchiramala <43574914+DylanonWicchiramala@users.noreply.github.com>
2020-10-24 16:07:39 +0700

    change regression to Regression , use pow(x, 2) instead of stupid x * x

--- regression.py
+++ regression.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pylab
 import statistics as stat
+import math
 from regression_lab.Sum import Sum
 
 DataSetxi = [0, 13, 22, 34, 52, 59, 76]
@@ -9,20 +10,21 @@ DataSetyi = [100, 96, 94, 91, 86, 83, 78]
 
 fig, ax = plt.subplots()
 
-class regression:
+
+class Regression:
 
     @staticmethod
     def linear_predict(val, axis=None):
         N = len(DataSetxi)
         w1 = (Sum.maltipy(DataSetxi, DataSetyi) - stat.mean(DataSetxi) * stat.mean(DataSetyi) * N) / \
-             (Sum.square(DataSetxi) - N * stat.mean(DataSetxi) * stat.mean(DataSetxi))
+             (Sum.square(DataSetxi) - N * math.pow(stat.mean(DataSetxi), 2))
         w0 = stat.mean(DataSetyi) - w1 * stat.mean(DataSetxi)
 
-        if (axis == 'x' or axis is None):
+        if axis == 'x' or axis is None:
             y = w1 * val + w0
             print("x is ", val, "predict y is ", y)
             return y
-        elif (axis == 'y'):
+        elif axis == 'y':
             x = (val - w0) / w1
             print("y is ", val, "predict x is", x)
             return x
@@ -31,7 +33,7 @@ class regression:
     def liner_plot(mx, my):
         N = len(mx)
         w1 = (Sum.maltipy(mx, my) - stat.mean(mx) * stat.mean(my) * N) / \
-             (Sum.square(mx) - N * stat.mean(mx) * stat.mean(mx))
+             (Sum.square(mx) - N * math.pow(stat.mean(mx), 2))
         w0 = stat.mean(my) - w1 * stat.mean(mx)
         x = np.linspace(min(mx) - 1, max(mx) + 2)
         y = w1 * x + w0
@@ -49,7 +51,7 @@ class regression:
 
 ax.grid(axis='both')
 
-regression.liner_plot(DataSetxi, DataSetyi)
-regression.data_plot(DataSetxi, DataSetyi)
-print(regression.linear_predict(0, axis='y')/60, "hrs.")
+Regression.liner_plot(DataSetxi, DataSetyi)
+Regression.data_plot(DataSetxi, DataSetyi)
+print(Regression.linear_predict(0, axis='y') / 60, "hrs.")
 pylab.show()
