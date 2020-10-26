cb5eded079eb8fe0636428942f7539b6234012fb
DylanonWicchiramala <43574914+DylanonWicchiramala@users.noreply.github.com>
2020-10-26 14:12:29 +0700

    add color agreement

--- regression2.py
+++ regression2.py
@@ -11,26 +11,24 @@ DataSetxi = np.array([0, 13, 22, 34, 52, 59, 76])
 DataSetyi = np.array([100, 99, 87, 88, 71, 83, 78])
 
 
-def data_plot(mx, my):
+def data_plot(mx, my, color='black'):
     plt.title(label='Regression')
-    plt.scatter(mx, my, color='black')
+    plt.scatter(mx, my, color=color)
 
 
-def regression_plot(mx, my, degree=None):
-    if degree is None: degree = 1
+def regression_plot(mx, my, degree=1, color=None):
     mx = mx.reshape((-1, 1))
     transformer = PolynomialFeatures(degree=degree, include_bias=False).fit(mx)
     mx_ = transformer.transform(mx)
     # mx_ = PolynomialFeatures(degree=degree, include_bias=False).fit_transform(mx)
     model = LinearRegression().fit(mx_, my)  # calculate weight value
-    x = np.linspace(min(mx), max(mx))
+    M = 5
+    x = np.linspace(min(mx) - M, max(mx) + M)
     data_plot(mx, my)
-    ax.plot(x, model.predict(transformer.transform(x)))
+    ax.plot(x, model.predict(transformer.transform(x)), color=color)
 
 
-def regrassion_predict(val, tx=None, ty=None, degree=None):
-    if degree is None: degree = 1
-    if tx is None or ty is None: tx = DataSetxi; ty = DataSetyi
+def regrassion_predict(val, tx=DataSetxi, ty=DataSetyi, degree=1):
     tx = tx.reshape((-1, 1))
     transformer = PolynomialFeatures(degree=degree, include_bias=False).fit(tx)
     tx_ = transformer.transform(tx)
@@ -38,7 +36,6 @@ def regrassion_predict(val, tx=None, ty=None, degree=None):
     model = LinearRegression().fit(tx_, ty)  # calculate weight value
     print("x is ", val, "predict y is ", *model.predict(transformer.transform(np.array(val).reshape(-1, 1))))
 
-for i in range(1, len(DataSetxi)):
-    regression_plot(DataSetxi, DataSetyi, i)
+regression_plot(DataSetxi, DataSetyi, 2)
 
 pylab.show()
